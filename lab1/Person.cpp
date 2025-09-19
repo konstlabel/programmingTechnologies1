b8d812db30b6865bafea392962b34d995b92cd35
konstlabel <kostiauvarov123@gmail.com>
2025-09-19 13:22:51 +0300

    Update Person

--- lab1/Person.cpp
+++ lab1/Person.cpp
@@ -4,24 +4,33 @@
 
 using namespace Persons;
 
+std::string Person::trim(const std::string& str) {
+    std::string trimmed = str;
+    trimmed.erase(0, trimmed.find_first_not_of(" \t\n\r"));
+    trimmed.erase(trimmed.find_last_not_of(" \t\n\r") + 1);
+    return trimmed;
+}
+
 Person::Person()
     : surname("unknown"), name("unknown"), patronymic("unknown") {
 
     std::cout << "The Person's default constructor is called" << std::endl;
 }
 
-Person::Person(const std::string& surname,
+Person::Person(int id,
+    const std::string& surname,
     const std::string& name,
-    const std::string& patronymic) {
+    const std::string& patronymic, Role role) : id(id) {
 
     setSurname(surname);
     setName(name);
     setPatronymic(patronymic);
+    setRole(role);
     std::cout << "The Person's parameterized constructor is called" << std::endl;
 }
 
 Person::Person(const Person& other)
-    : surname(other.surname), name(other.name), patronymic(other.patronymic) {
+    : id(other.id), surname(other.surname), name(other.name), patronymic(other.patronymic), role(other.role) {
 
     std::cout << "The Person's copy constructor is called" << std::endl;
 }
@@ -35,11 +44,18 @@ Person& Person::operator=(const Person& other) {
 
     if (this == &other) return *this;
 
-    this->surname = other.surname;
-    this->name = other.name;
-    this->patronymic = other.patronymic;
+    id = other.id;
+    surname = other.surname;
+    name = other.name;
+    patronymic = other.patronymic;
+	role = other.role;
 
     return *this;
+}
+
+int Person::getId() const {
+
+    return id;
 }
 
 const std::string& Person::getSurname() const {
@@ -64,40 +80,48 @@ const Role& Person::getRole() const {
 
 void Person::setSurname(const std::string& surname) {
 
-    std::string trimmed = surname;
-    trimmed.erase(0, trimmed.find_first_not_of(" \t\n\r"));
-    trimmed.erase(trimmed.find_last_not_of(" \t\n\r") + 1);
+	std::string trimmed = trim(surname);
 
     if (trimmed.empty())
-        this->surname = "unknown";
-    else
-        this->surname = trimmed;
+        throw std::invalid_argument("Error! Empty string is not allowed");
+    
+    this->surname = trimmed;
 }
 
 void Person::setName(const std::string& name) {
     
-    std::string trimmed = name;
-    trimmed.erase(0, trimmed.find_first_not_of(" \t\n\r"));
-    trimmed.erase(trimmed.find_last_not_of(" \t\n\r") + 1);
+	std::string trimmed = trim(name);
 
     if (trimmed.empty())
-        this->name = "unknown";
-    else
-        this->name = trimmed;
+        throw std::invalid_argument("Error! Empty string is not allowed");
+        
+    this->name = trimmed;
 }
 void Person::setPatronymic(const std::string& patronymic) {
 
-    std::string trimmed = patronymic;
-    trimmed.erase(0, trimmed.find_first_not_of(" \t\n\r"));
-    trimmed.erase(trimmed.find_last_not_of(" \t\n\r") + 1);
+	std::string trimmed = trim(patronymic);
 
     if (trimmed.empty())
-        this->patronymic = "unknown";
-    else
-        this->patronymic = trimmed;
+        throw std::invalid_argument("Error! Empty string is not allowed");
+        
+    this->patronymic = trimmed;
 }
 
 void Person::setRole(Role role) {
 
     this->role = role;
+}
+
+
+void Person::clearSurname() {
+    surname = "unknown";
+}
+
+void Person::clearName() {
+
+    name = "unknown";
+}
+
+void Person::clearPatronymic() {
+    patronymic = "unknown";
 }
--- lab1/Person.h
+++ lab1/Person.h
@@ -6,20 +6,23 @@
 namespace Persons {
     class Person {
     protected:
+        int id;
         std::string surname;
         std::string name;
         std::string patronymic;
         Role role;
 
+		static std::string trim(const std::string& str);
     public:
         Person();
-        explicit Person(const std::string& surname, const std::string& name, const std::string& patronymic);
+        explicit Person(int id, const std::string& surname, const std::string& name, const std::string& patronymic, Role role);
         Person(const Person& other);
 
         virtual ~Person();
         
         Person& operator =(const Person& other);
 
+        int getId() const;
         const std::string& getSurname() const;
         const std::string& getName() const;
         const std::string& getPatronymic() const;
@@ -29,6 +32,10 @@ namespace Persons {
         void setName(const std::string& name);
         void setPatronymic(const std::string& patronymic);
         virtual void setRole(Role role);
+
+        void clearSurname();
+        void clearName();
+        void clearPatronymic();
 
         virtual std::string toString() const = 0;
     };
